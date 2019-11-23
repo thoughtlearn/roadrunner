67db5e26b256b1fef2753846087ea2b7f5aa728a
Sanjeev S <sanjeevs@thoughtworks.com>
2019-11-23 10:04:39 +0530

    Initial setup changes

---
+++ src/Garage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+function Garage(props) {
+   const vehicles = props.vehicles.map( v => {
+
+    });
+    return (
+        <div className="garage">
+
+        </div>
+    );
+}
+
+export default Garage;
